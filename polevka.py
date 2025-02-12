c2756738e6a2c9946e4419ba06677f9573a236be
standa2 <stana121@trebesin.cz>
2025-02-12 08:52:34 +0100

    hihihihhihiih

--- polevka.py
+++ polevka.py
@@ -1,13 +1,34 @@
 from bs4 import BeautifulSoup
 import requests
-
+import json
 
 def main():
+    url = "https://js-trebesin.github.io/bsoup-exam/"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser") 
+    # <--- Úkol: popiš krátce, co tohle dělá
+    # Je to funkce která přeloží stránku pro kód do html
+
+    cesnecka = soup.find_all("h2")
+
+    list = []
+    finale = []
 
 
-    # response = requests.get(url)
+    for ingredient in cesnecka:
+        list.append(ingredient.text)
 
-    # BeautifulSoup(response.content, "html.parser") <--- Úkol: popiš krátce, co tohle dělá
+    finale.append(list[0])
+    finale.append(list[1])
+    finale.append(list[2])
+    finale.append(list[3])
+
+    print(finale)
+
+    with open("recept.json", mode="w") as file:
+        json.dump(finale, file)
+ 
+
 
 
 if __name__ == "__main__":
--- secret.py
+++ secret.py
@@ -6,16 +6,26 @@ import os
 
 def main():
 
-    # with sync_playwright() as p:
+    with sync_playwright() as p:
+        meno = "Jarmil"
+        heso = "Admin123"
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
 
-        # browser = p.chromium.launch(headless=False)
-
-        # !!!
-        # na page.locator(selector) se dá použít funkce .text_content(), která vypíše text daného prvku
-        # !!!
+        page.goto("https://js-trebesin.github.io/playwright-exam/")
 
 
-        # browser.close()
+        page.fill('input[id="login"]', meno)
+        page.fill('input[id="pass"]', heso)
+
+        page.click('button[class="login-btn"]')
+
+        secret = page.locator('p[class="super-secret-text"]')
+
+        print(secret.text_content())
+
+        input("dobre ranko prosim jednicku s hvezdickou")
+        browser.close()
     
 
 if __name__ == "__main__":
